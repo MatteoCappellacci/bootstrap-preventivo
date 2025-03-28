b1903b44a6478772a6dcf5089d43a7b3744d5518
MatteoCappellacci <Matteo.cappellacci1987@gmail.com>
2025-03-28 18:26:32 +0100

    feat: quasi completo

--- js/script.js
+++ js/script.js
@@ -19,22 +19,51 @@ const codicePromo = userCodicePromo.value;
 const sviluppoBackend = 20.50 * 10;
 const sviluppoFrontend = 15.30 * 10;
 const analisiProgettuale = 33.60 * 10;
-const codiciPromo = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"
+const codiciPromoArr = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"
 ];
 
-let risultato
-let sconto1
-let sconto2
-let sconto3
+let risultato = 0;  
+let sconto1 = 0 ;
+let sconto2 = 0;
+let sconto3 = 0;
 
 /*esecuzione logica if else*/
-if (codiciPromo.includes(userCodicePromo) === true) {
+if (codiciPromoArr.includes(codicePromo) === true) {
 
     /* inserire il tipo sconto nella variabili sconto poi sempore con if else inserire il valore di "risultato" a seconda del "tipoLavoro" scelto(option) */
+    sconto1 = (sviluppoBackend / 100) * 25;
+    sconto2 = (sviluppoFrontend / 100 ) * 25;
+    sconto3 = (analisiProgettuale / 100) * 25
+
+}
+
+if ( tipoLavoro === "Back-Dev") {
+    risultato = sviluppoBackend - sconto1;}
+    else if ( tipoLavoro === "Front-End") {
+        risultato = sviluppoFrontend - sconto2;
+    }
+    else if (tipoLavoro === "Proj-Analyst"){
+        risultato = analisiProgettuale - sconto3
+    }
+    else {
+    alert ("Errore: Seleziona tipo di lavoro!")
+    }
+    /*prezzoFinale.innerText = risultato*/
+    /*output*/
+    const rislutatoFormattato = risultato.toFixed(2).replace(".",",");
+    const index = rislutatoFormattato.indexOf(",")
+
+    const risultato1 = rislutatoFormattato.slice(0,index);
+    const risultato2 = rislutatoFormattato.slice(index)
+    
+    prezzoFinale.innerHTML =  `€ ${risultato1}<span class="text-secondary fw-normal fs-3">${risultato2}</span>` 
+
+    prezzoFinale.innerText = `€ ${risultato.toFixed(2).replace(".", ",")}`
+
 }
 
 
-}
+
 
 
 
